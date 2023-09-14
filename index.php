686e4a551f7283c299738f2a352025692850981f
Driginbrn <misatalic@gmail.com>
2023-09-15 01:25:41 +0200

    Validacija JS

--- index.php
+++ index.php
@@ -40,34 +40,35 @@
             <!-- Ime i prezime -->
             <div class="mb-3">
                 <label for="nosilac_osiguranja" class="form-label">Ime i prezime</label>*
-                <input type="text" class="form-control" maxlength="50" name="nosilac_osiguranja" value="<?php echo isset($_POST['nosilac_osiguranja']) ? htmlspecialchars($_POST['nosilac_osiguranja']) : ''; ?>" required>
+                <input type="text" class="form-control" maxlength="50" name="nosilac_osiguranja">
                 <div class="error" id="nosilac_osiguranja_error" style="color: red;"></div>
             </div>
 
             <!-- Datum rodjenja -->
             <div class="mb-3">
                 <label for="datum_rodjenja" class="form-label">Datum rođenja</label>*
-                <input type="date" class="form-control" placeholder="dd-mm-yyyy" name="datum_rodjenja" value="<?php echo isset($_POST['datum_rodjenja']) ? htmlspecialchars($_POST['datum_rodjenja']) : ''; ?>" required>
+                <input type="date" class="form-control" placeholder="dd-mm-yyyy" name="datum_rodjenja">
+                <div class="error" id="datum_rodjenja_error" style="color: red;"></div>
             </div>
 
             <!-- Broj pasosa -->
             <div class="mb-3">
                 <label for="broj_pasosa" class="form-label">Broj pasoša</label>*
-                <input type="tel" class="form-control" maxlength="20" name="broj_pasosa" value="<?php echo isset($_POST['broj_pasosa']) ? htmlspecialchars($_POST['broj_pasosa']) : ''; ?>" required>
+                <input type="text" class="form-control" name="broj_pasosa">
                 <div class="error" id="broj_pasosa_error" style="color: red;"></div>
             </div>
 
             <!-- Broj telefona -->
             <div class="mb-3">
                 <label for="telefon" class="form-label">Telefon</label>
-                <input type="text" class="form-control" name="telefon" maxlength="10" value="<?php echo isset($_POST['telefon']) ? htmlspecialchars($_POST['telefon']) : ''; ?>">
+                <input type="text" class="form-control" name="telefon" maxlength="10">
                 <div class="error" id="telefon_error" style="color: red;"></div>
             </div>
 
             <!-- Email -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>*
-                <input type="email" class="form-control" name="email" aria-describedby="emailHelp" maxlength="30" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
+                <input type="email" class="form-control" name="email" aria-describedby="emailHelp" maxlength="30">
                 <div class="error" id="email_error" style="color: red;"></div>
             </div>
 
@@ -76,12 +77,13 @@
 
                 <!-- Datum od -->
                 <label for="datum_putovanja_od" class="form-label">Datum putovanja (OD)</label>*
-                <input type="date" name="datum_putovanja_od" class="form-control" placeholder="dd-mm-yyyy" min="1997-01-01" max="2030-12-31" value="<?php echo isset($_POST['datum_putovanja_od']) ? htmlspecialchars($_POST['datum_putovanja_od']) : ''; ?>" required>
+                <input type="date" name="datum_putovanja_od" class="form-control" placeholder="dd-mm-yyyy" min="1997-01-01" max="2030-12-31">
 
                 <!-- Datum do -->
                 <label for="datum_putovanja_do" class="form-label">Datum putovanja (DO)</label>*
-                <input type="date" name="datum_putovanja_do" class="form-control" placeholder="dd-mm-yyyy" min="1997-01-01" max="2030-12-31" value="<?php echo isset($_POST['datum_putovanja_do']) ? htmlspecialchars($_POST['datum_putovanja_do']) : ''; ?>" required>
+                <input type="date" name="datum_putovanja_do" class="form-control" placeholder="dd-mm-yyyy" min="1997-01-01" max="2030-12-31">
                 <div class="error" id="datum_putovanja_error" style="color: red;"></div>
+                <div class="error" id="broj_dana_error" style="color: red;"></div>
 
                 <p>Broj dana u odnosu na odabrani datum: <span id="broj_dana"></span></p>
             </div>
@@ -116,165 +118,85 @@
 
             <br><br>
 
-            
-            <!-- PHP -->
-            <?php
-
-        // Konekcija sa bazom
-        $mysqli = new mysqli("localhost", "root", "", "putnoosiguranje");
-
-        // Dodaj novo osiguranje
-        if (isset($_POST['dodaj'])) {
-            if ((!$_POST['nosilac_osiguranja']) || (!$_POST['datum_rodjenja']) || (!$_POST['broj_pasosa']) || (!$_POST['email']) || (!$_POST['datum_putovanja_od']) || (!$_POST['datum_putovanja_do']) || (!$_POST['vrsta_polise'])) {
-            echo "Morate popuniti sva polja!<br>";
-        } else {
-            $nosilac_osiguranja = $_POST['nosilac_osiguranja'];
-            $datum_rodjenja = $_POST['datum_rodjenja'];
-            $broj_pasosa = $_POST['broj_pasosa'];
-            $telefon = $_POST['telefon'];
-            $email = $_POST['email'];
-            $datum_putovanja_od = $_POST['datum_putovanja_od'];
-            $datum_putovanja_do = $_POST['datum_putovanja_do'];
-            $vrsta_polise = $_POST['vrsta_polise'];
-
-            $errors = [];
-
-            // Validacija nosioca osiguranja (samo slova)
-            if (!preg_match('/^[a-zA-Z]+$/', $nosilac_osiguranja)) {
-                $errors['nosilac_osiguranja'] = 'Ime i prezime moraju sadržati samo slova.';
-            }
-
-            // Validacija broja pasoša (samo brojevi)
-            if (!is_numeric($broj_pasosa)) {
-                $errors['broj_pasosa'] = 'Broj pasoša mora sadržati samo brojeve.';
-            }
-
-            // Validacija telefona (opciono, ako je popunjen)
-            if (!empty($telefon) && !is_numeric($telefon)) {
-                $errors['telefon'] = 'Telefon mora sadržati samo brojeve.';
-            }
-
-            // Validacija email adrese
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $errors['email'] = 'Email adresa nije ispravna.';
-            }
-
-            if (strtotime($datum_putovanja_od) >= strtotime($datum_putovanja_do)) {
-                $errors['datum_putovanja'] = 'Datum putovanja do mora biti veći od datuma putovanja od.';
-            }
-
-            // Prikazivanje poruka o grešci ispod odgovarajućih inputa
-            if (!empty($errors)) {
-            foreach ($errors as $field => $error) {
-                echo '<script>document.getElementById("'.$field.'_error").innerHTML = "'.$error.'";</script>';
-            }
-            die($mysqli->error);
-            }
-
-            $sql_dodaj = "INSERT INTO osiguranje (nosilac_osiguranja, datum_rodjenja, broj_pasosa, telefon, email, datum_putovanja_od, datum_putovanja_do, vrsta_polise)";
-            $sql_dodaj .= "VALUES ('$nosilac_osiguranja', '$datum_rodjenja', '$broj_pasosa', '$telefon', '$email', '$datum_putovanja_od', '$datum_putovanja_do', '$vrsta_polise')";
-            $res_dodaj = $mysqli->query($sql_dodaj);
-
-            if (!$res_dodaj) {
-                echo "Greska pri izvrsavanju upita dodavanja!";
-                die($mysqli->error);
-            }
-            echo "<p class=\"h5\">Osiguranje je uspešno dodato!</p><br>";
-        }
-    }
-
-        // Dodaj novo grupno osiguranje
-        if (isset($_POST['dodaj_dodatno'])) {
-            if ((!$_POST['nosilac_osiguranja']) || (!$_POST['datum_rodjenja']) || (!$_POST['broj_pasosa']) || (!$_POST['email']) || 
-            (!$_POST['datum_putovanja_od']) || (!$_POST['datum_putovanja_do']) || (!$_POST['vrsta_polise']) || (!$_POST['d_nosilac_osiguranja']) || (!$_POST['d_datum_rodjenja']) || (!$_POST['d_broj_pasosa']) ) {
-            echo "Morate popuniti sva polja!<br>";
-        } else {
-            $nosilac_osiguranja = $_POST['nosilac_osiguranja'];
-            $datum_rodjenja = $_POST['datum_rodjenja'];
-            $broj_pasosa = $_POST['broj_pasosa'];
-            $telefon = $_POST['telefon'];
-            $email = $_POST['email'];
-            $datum_putovanja_od = $_POST['datum_putovanja_od'];
-            $datum_putovanja_do = $_POST['datum_putovanja_do'];
-            $vrsta_polise = $_POST['vrsta_polise'];
-
-            $d_nosilac_osiguranja = $_POST['d_nosilac_osiguranja'];
-            $d_datum_rodjenja = $_POST['d_datum_rodjenja'];
-            $d_broj_pasosa = $_POST['d_broj_pasosa'];
-
-
-            $errors = [];
-
-            // Validacija nosioca osiguranja (samo slova)
-            if (!preg_match('/^[a-zA-Z\s]+$/', $nosilac_osiguranja)) {
-                $errors['nosilac_osiguranja'] = 'Ime i prezime moraju sadržati samo slova.';
-            }
-
-            // Validacija broja pasoša (samo brojevi)
-            if (!is_numeric($broj_pasosa)) {
-                $errors['broj_pasosa'] = 'Broj pasoša mora sadržati samo brojeve.';
-            }
-
-            // Validacija telefona (opciono, ako je popunjen)
-            if (!empty($telefon) && !is_numeric($telefon)) {
-                $errors['telefon'] = 'Telefon mora sadržati samo brojeve.';
-            }
-
-            // Validacija email adrese
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $errors['email'] = 'Email adresa nije ispravna.';
-            }
-
-            if (strtotime($datum_putovanja_od) >= strtotime($datum_putovanja_do)) {
-                $errors['datum_putovanja'] = 'Datum putovanja do mora biti veći od datuma putovanja od.';
-            }
-
-            // Validacija dodatnog osiguranika (samo slova)
-            if (!preg_match('/^[a-zA-Z\s]+$/', $nosilac_osiguranja)) {
-                $errors['d_nosilac_osiguranja'] = 'Ime i prezime moraju sadržati samo slova.';
-            }
-
-            // Validacija dodatnog broja pasoša (samo brojevi)
-            if (!is_numeric($broj_pasosa)) {
-                $errors['d_broj_pasosa'] = 'Broj pasoša mora sadržati samo brojeve.';
-            }
-
-            // Prikazivanje poruka o grešci ispod odgovarajućih inputa
-            if (!empty($errors)) {
-            foreach ($errors as $field => $error) {
-                echo '<script>document.getElementById("'.$field.'_error").innerHTML = "'.$error.'";</script>';
-            }
-            die($mysqli->error);
-            }
-
-            $sql_dodaj = "INSERT INTO osiguranje (nosilac_osiguranja, datum_rodjenja, broj_pasosa, telefon, email, datum_putovanja_od, datum_putovanja_do, vrsta_polise)";
-            $sql_dodaj .= "VALUES ('$nosilac_osiguranja', '$datum_rodjenja', '$broj_pasosa', '$telefon', '$email', '$datum_putovanja_od', '$datum_putovanja_do', '$vrsta_polise')";
-            $res_dodaj = $mysqli->query($sql_dodaj);
-
-            if (!$res_dodaj) {
-                echo "Greska pri izvrsavanju upita dodavanja!";
-                die($mysqli->error);
-            }
-
-            $sql_dodaj_dodatno = "INSERT INTO dodatni_osiguranik (d_nosilac_osiguranja, d_datum_rodjenja, d_broj_pasosa, glavni_osiguranik_id)";
-            $sql_dodaj_dodatno .= "VALUES ('$d_nosilac_osiguranja', '$d_datum_rodjenja', '$d_broj_pasosa', '$broj_pasosa')";
-            $res_dodaj_dodatno = $mysqli->query($sql_dodaj_dodatno);
-
-            if (!$res_dodaj) {
-                echo "Greska pri izvrsavanju upita dodavanja!";
-                die($mysqli->error);
-            }
-
-            echo "<p class=\"h5\">Grupno osiguranje je uspešno dodato!</p><br>";
-            }
-        }
-
-        ?>
-
         </form>
     </div>
 
     <script src="js/script.js"></script>
+
+    <!-- PHP -->
+    <?php
+
+// Konekcija sa bazom
+$mysqli = new mysqli("localhost", "root", "", "putnoosiguranje");
+
+// Dodaj novo osiguranje
+if (isset($_POST['dodaj'])) {
+    if ((!$_POST['nosilac_osiguranja']) || (!$_POST['datum_rodjenja']) || (!$_POST['broj_pasosa']) || (!$_POST['email']) || (!$_POST['datum_putovanja_od']) || (!$_POST['datum_putovanja_do']) || (!$_POST['vrsta_polise'])) {
+    echo "Morate popuniti sva polja!<br>";
+} else {
+    $nosilac_osiguranja = $_POST['nosilac_osiguranja'];
+    $datum_rodjenja = $_POST['datum_rodjenja'];
+    $broj_pasosa = $_POST['broj_pasosa'];
+    $telefon = $_POST['telefon'];
+    $email = $_POST['email'];
+    $datum_putovanja_od = $_POST['datum_putovanja_od'];
+    $datum_putovanja_do = $_POST['datum_putovanja_do'];
+    $vrsta_polise = $_POST['vrsta_polise'];
+
+    $sql_dodaj = "INSERT INTO osiguranje (nosilac_osiguranja, datum_rodjenja, broj_pasosa, telefon, email, datum_putovanja_od, datum_putovanja_do, vrsta_polise)";
+    $sql_dodaj .= "VALUES ('$nosilac_osiguranja', '$datum_rodjenja', '$broj_pasosa', '$telefon', '$email', '$datum_putovanja_od', '$datum_putovanja_do', '$vrsta_polise')";
+    $res_dodaj = $mysqli->query($sql_dodaj);
+
+    if (!$res_dodaj) {
+        echo "Greska pri izvrsavanju upita dodavanja!";
+        die($mysqli->error);
+    }
+    echo "<p class=\"h5\">Osiguranje je uspešno dodato!</p><br>";
+}
+}
+
+// Dodaj novo grupno osiguranje
+if (isset($_POST['dodaj_dodatno'])) {
+    if ((!$_POST['nosilac_osiguranja']) || (!$_POST['datum_rodjenja']) || (!$_POST['broj_pasosa']) || (!$_POST['email']) || 
+    (!$_POST['datum_putovanja_od']) || (!$_POST['datum_putovanja_do']) || (!$_POST['vrsta_polise']) || (!$_POST['d_nosilac_osiguranja']) || (!$_POST['d_datum_rodjenja']) || (!$_POST['d_broj_pasosa']) ) {
+    echo "Morate popuniti sva polja!<br>";
+} else {
+    $nosilac_osiguranja = $_POST['nosilac_osiguranja'];
+    $datum_rodjenja = $_POST['datum_rodjenja'];
+    $broj_pasosa = $_POST['broj_pasosa'];
+    $telefon = $_POST['telefon'];
+    $email = $_POST['email'];
+    $datum_putovanja_od = $_POST['datum_putovanja_od'];
+    $datum_putovanja_do = $_POST['datum_putovanja_do'];
+    $vrsta_polise = $_POST['vrsta_polise'];
+
+    $d_nosilac_osiguranja = $_POST['d_nosilac_osiguranja'];
+    $d_datum_rodjenja = $_POST['d_datum_rodjenja'];
+    $d_broj_pasosa = $_POST['d_broj_pasosa'];
+
+    $sql_dodaj = "INSERT INTO osiguranje (nosilac_osiguranja, datum_rodjenja, broj_pasosa, telefon, email, datum_putovanja_od, datum_putovanja_do, vrsta_polise)";
+    $sql_dodaj .= "VALUES ('$nosilac_osiguranja', '$datum_rodjenja', '$broj_pasosa', '$telefon', '$email', '$datum_putovanja_od', '$datum_putovanja_do', '$vrsta_polise')";
+    $res_dodaj = $mysqli->query($sql_dodaj);
+
+    if (!$res_dodaj) {
+        echo "Greska pri izvrsavanju upita dodavanja!";
+        die($mysqli->error);
+    }
+
+    $sql_dodaj_dodatno = "INSERT INTO dodatni_osiguranik (d_nosilac_osiguranja, d_datum_rodjenja, d_broj_pasosa, glavni_osiguranik_id)";
+    $sql_dodaj_dodatno .= "VALUES ('$d_nosilac_osiguranja', '$d_datum_rodjenja', '$d_broj_pasosa', '$broj_pasosa')";
+    $res_dodaj_dodatno = $mysqli->query($sql_dodaj_dodatno);
+
+    if (!$res_dodaj) {
+        echo "Greska pri izvrsavanju upita dodavanja!";
+        die($mysqli->error);
+    }
+
+    echo "<p class=\"h5\">Grupno osiguranje je uspešno dodato!</p><br>";
+    }
+}
+
+?>
 
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-// Računanje broja dana u odnosu na odabrane datume
-const datumOdInput = document.querySelector('input[name="datum_putovanja_od"]');
-const datumDoInput = document.querySelector('input[name="datum_putovanja_do"]');
-const brojDanaSpan = document.getElementById('broj_dana');
-
-datumOdInput.addEventListener('change', updateBrojDana);
-datumDoInput.addEventListener('change', updateBrojDana);
-
-function updateBrojDana() {
-    const datumOd = new Date(datumOdInput.value);
-    const datumDo = new Date(datumDoInput.value);
-    const razlika = Math.ceil((datumDo - datumOd) / (1000 * 60 * 60 * 24));
-    if(razlika == 1){
-        brojDanaSpan.textContent = razlika + ' dan';
-    }else{
-
-    brojDanaSpan.textContent = razlika + ' dana';
-    }
-}
-
 // Dodatni osiguranik
 function dodajOsiguranika() {
     const osiguraniciDiv = document.getElementById('osiguranici');
@@ -30,7 +10,6 @@ function dodajOsiguranika() {
             <div class="mb-3">
                 <label for="nosilac_osiguranja" class="form-label">Ime i prezime</label>*
                 <input type="text" class="form-control" name="d_nosilac_osiguranja" required>
-                <div class="error" id="d_nosilac_osiguranja_error" style="color: red;"></div>
             </div>
 
             <!-- Datum rodjenja -->
@@ -43,7 +22,6 @@ function dodajOsiguranika() {
             <div class="mb-3">
                 <label for="broj_pasosa" class="form-label">Broj pasoša</label>*
                 <input type="tel" class="form-control" name="d_broj_pasosa" required>
-                <div class="error" id="d_broj_pasosa" style="color: red;"></div>
             </div>
 
             <!-- Button za brisanje -->
@@ -89,3 +67,137 @@ if (invidualnoRadio.checked) {
     potvrdiBtn.setAttribute('name', "dodaj");
 }});
 
+// Validacija
+document.addEventListener('DOMContentLoaded', function () {
+    const formaUnos = document.getElementById('forma_unos');
+    const nosilacOsiguranjaInput = document.querySelector('input[name="nosilac_osiguranja"]');
+    const datumRodjenjaInput = document.querySelector('input[name="datum_rodjenja"]');
+    const brojPasosaInput = document.querySelector('input[name="broj_pasosa"]');
+    const telefonInput = document.querySelector('input[name="telefon"]');
+    const emailInput = document.querySelector('input[name="email"]');
+    const datumOdInput = document.querySelector('input[name="datum_putovanja_od"]');
+    const datumDoInput = document.querySelector('input[name="datum_putovanja_do"]');
+    const brojDanaSpan = document.getElementById('broj_dana');
+    const datumPutovanjaError = document.getElementById('datum_putovanja_error');
+    const brojDanaError = document.getElementById('broj_dana_error');
+    const nosilacOsiguranjaError = document.getElementById('nosilac_osiguranja_error');
+    const datumRodjenjaError = document.getElementById('datum_rodjenja_error');
+    const brojPasosaError = document.getElementById('broj_pasosa_error');
+    const telefonError = document.getElementById('telefon_error');
+    const emailError = document.getElementById('email_error');
+
+    formaUnos.addEventListener('submit', function (event) {
+
+        // Validacija za nosioca osiguranja
+        const nosilacOsiguranjaValue = nosilacOsiguranjaInput.value.trim();
+        const nosilacOsiguranjaRegex = /^[A-Z][a-z]+ [A-Z][a-z]+$/;
+
+        if (!nosilacOsiguranjaValue) {
+            nosilacOsiguranjaError.textContent = 'Polje "Ime i prezime" ne sme biti prazno.';
+            console.log('Nosilac osiguranja: Polje ne sme biti prazno.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else if (!nosilacOsiguranjaRegex.test(nosilacOsiguranjaValue) || nosilacOsiguranjaValue.length > 50) {
+            nosilacOsiguranjaError.textContent = 'Unesite ime i prezime u formatu "Ime Prezime" sa početnim velikim slovima i ne više od 50 karaktera.';
+            console.log('Nosilac osiguranja: Unesite ime i prezime u formatu "Ime Prezime" sa početnim velikim slovima i ne više od 50 karaktera.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else {
+            nosilacOsiguranjaError.textContent = '';
+        }
+
+        // Validacija za datum rođenja
+        const datumRodjenjaValue = datumRodjenjaInput.value;
+        if (!datumRodjenjaValue) {
+            datumRodjenjaError.textContent = 'Morate odabrati datum rođenja.';
+            console.log('Datum rodjenja: Morate odabrati datum rođenja.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else {
+            datumRodjenjaError.textContent = '';
+        }
+
+        // Validacija za broj pasoša
+        const brojPasosaValue = brojPasosaInput.value.trim();
+        if (!/^\d{1,20}$/.test(brojPasosaValue)) {
+            brojPasosaError.textContent = 'Broj pasoša mora sadržati samo brojeve i ne sme biti duži od 20 karaktera.';
+            console.log('Broj pasoša: Broj pasoša mora sadržati samo brojeve i ne sme biti duži od 20 karaktera.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else {
+            brojPasosaError.textContent = '';
+        }
+
+        // Validacija za telefon
+        const telefonValue = telefonInput.value.trim();
+        if (telefonValue && (!/^\d+$/.test(telefonValue) || telefonValue.length > 10)) {
+            telefonError.textContent = 'Telefon može sadržati samo do 10 brojeva.';
+            console.log('Telefon: Telefon može sadržati samo do 10 brojeva.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else {
+            telefonError.textContent = '';
+        }
+
+        // Validacija za email
+        const emailValue = emailInput.value.trim();
+        if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(emailValue) || emailValue.length > 30) {
+            emailError.textContent = 'Molimo unesite validnu email adresu koja ne sme biti duža od 30 karaktera.';
+            console.log('Telefon: Telefon može sadržati samo do 10 brojeva.');
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            return;
+        } else {
+            emailError.textContent = '';
+        }
+
+        // Validacija za datume putovanja
+        const datumOdValue = new Date(datumOdInput.value);
+        const datumDoValue = new Date(datumDoInput.value);
+
+        if (!isNaN(datumOdValue.getTime()) && !isNaN(datumDoValue.getTime()) && datumDoValue >= datumOdValue) {
+            const razlika = Math.ceil((datumDoValue - datumOdValue) / (1000 * 60 * 60 * 24));
+            if (razlika < 0) {
+                datumPutovanjaError.textContent = 'Datum "DO" ne sme biti pre datuma "OD".';
+                event.preventDefault(); // Zaustavljanje podnošenja forme
+                brojDanaError.textContent = '';
+                return;
+            } else {
+                datumPutovanjaError.textContent = '';
+                if (razlika == 1) {
+                    brojDanaSpan.textContent = razlika + ' dan';
+                } else {
+                    brojDanaSpan.textContent = razlika + ' dana';
+                }
+            }
+        } else {
+            datumPutovanjaError.textContent = 'Morate ispravno odabrati datume.';
+            event.preventDefault(); // Zaustavljanje podnošenja forme
+            brojDanaSpan.textContent = '';
+            return;
+        }
+
+        // Ako su sve validacije prošle, možete omogućiti slanje forme na server
+        formaUnos.submit();
+        console.log('Form Submit');
+    });
+
+    // Računanje broja dana pri promeni datuma
+    datumOdInput.addEventListener('change', updateBrojDana);
+    datumDoInput.addEventListener('change', updateBrojDana);
+
+    function updateBrojDana() {
+        const datumOd = new Date(datumOdInput.value);
+        const datumDo = new Date(datumDoInput.value);
+
+        if (!isNaN(datumOd.getTime()) && !isNaN(datumDo.getTime()) && datumDo >= datumOd) {
+            const razlika = Math.ceil((datumDo - datumOd) / (1000 * 60 * 60 * 24));
+            if (razlika == 1) {
+                brojDanaSpan.textContent = razlika + ' dan';
+            } else {
+                brojDanaSpan.textContent = razlika + ' dana';
+            }
+        } else {
+            brojDanaSpan.textContent = '';
+        }
+    }
+});
